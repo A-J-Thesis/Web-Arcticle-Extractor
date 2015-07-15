99d87ceb8a6b1c561ea2392783cc1cac79d10b8e
Aris Kots <ArisKots1992@gmail.com>
2015-07-16 00:28:54 +0300

    Update Site.java

--- NewsCrawler/src/Crawler/Site.java
+++ NewsCrawler/src/Crawler/Site.java
@@ -1,3 +1,7 @@
+// Project:  World News Articles Matching
+// File:     Site.java
+// Authors:  Jason Papapanagiotakis, Aris Kotsomitopoulos
+// Github:   https://github.com/A-J-Thesis/Web-Arcticle-Extractor
 package Crawler;
 
 import java.io.File;
